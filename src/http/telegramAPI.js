07a0af5a710ac3e7f1e0799c5652635af67180a8
Виктор <viktor@MacBook-Air-Viktor.local>
2023-09-24 20:46:02 +0300

    3

--- src/http/telegramAPI.js
+++ src/http/telegramAPI.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-// const token = '5562126487:AAGqX2TBd3toX15OgSCQ2yW55RNfgtBWQko'
-// const chat_id = '-1001794221917'
+const token = '5562126487:AAGqX2TBd3toX15OgSCQ2yW55RNfgtBWQko'
+const chat_id = '-1001794221917'
 // const uri_api = `https://api.telegram.org/bot${token}/sendMessage`
-const token = '6036773812:AAEeJcl1NyW0PwEf4Mz7DsxKWew5hd9jTSM'
-const chat_id = '-1001985636526'
+// const token = '6036773812:AAEeJcl1NyW0PwEf4Mz7DsxKWew5hd9jTSM'
+// const chat_id = '-1001985636526'
 const uri_api = `https://api.telegram.org/bot${token}/sendMessage`
 
 
